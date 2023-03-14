19b858c0fa2cc8a389b2c85c240d7d8575512bbe
Vera Maria <mymadonna89@mail.ru>
2023-03-14 21:01:31 +0600

    Homework 64 solved

--- Homework_64/Program.cs
+++ Homework_64/Program.cs
@@ -1,4 +1,20 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
-//промежутке от N до 1. Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿static void Print(string word)
+{
+    Console.WriteLine(word);
+}
+Print("Вывести все натуральные числа в промежутке от N до 1.");
+
+Random rand = new Random();
+int numberN = rand.Next(2, 100);
+Decrease(numberN);
+
+
+static void Decrease(int numberN)
+{
+    if (1 <= numberN)
+    {
+        System.Console.WriteLine(numberN);
+        numberN--;
+        Decrease(numberN);
+    }
+}
--- Homework_66/Program.cs
+++ Homework_66/Program.cs
@@ -2,4 +2,25 @@
 // элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
- 
+
+static void Print(string word)
+{
+    Console.WriteLine(word);
+}
+
+Print("Найти сумму чисел от M до N.");
+Print("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+//rint("Введите число N");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+ Summa(numberM);
+
+
+static int Summa(int numberM)
+{
+    if (numberM <= 8)
+        return 0;
+    int t = 0;
+    return t + Summa(numberM);
+    Console.WriteLine($"Для числа {numberM}\t" + "суммой чисел будет являться\t");
+}
